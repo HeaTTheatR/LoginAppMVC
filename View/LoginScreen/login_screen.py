c7f83efe8801061e9666d07a990f1e967f4d51c5
macbookair <kivydevelopment@gmail.com>
2021-10-01 17:23:01 +0300

    Replace `Toast` to `Snackbar` class

--- View/LoginScreen/login_screen.py
+++ View/LoginScreen/login_screen.py
@@ -1,15 +1,17 @@
-from typing import NoReturn
+from typing import NoReturn, Union
 
+from kivy.clock import Clock
 from kivy.properties import ObjectProperty
 
-from kivymd.toast import toast
+from kivymd.theming import ThemableBehavior
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.snackbar import Snackbar
 
 from Utility.observer import Observer
 
 
-class LoginScreenView(MDScreen, Observer):
+class LoginScreenView(ThemableBehavior, MDScreen, Observer):
     """
     A class that implements a visual representation of the model data
     :class:`~Model.login_screen.LoginScreenModel`.
@@ -54,6 +56,15 @@ class LoginScreenView(MDScreen, Observer):
         self.dialog.text = "Data validation..."
         self.dialog.open()
 
+    def show_toast(self, interval: Union[int, float]) -> NoReturn:
+        Snackbar(
+            text="You have passed the verification successfully!",
+            snackbar_x="10dp",
+            snackbar_y="10dp",
+            size_hint_x=.8,
+            bg_color=self.theme_cls.primary_color,
+        ).open()
+
     def model_is_changed(self) -> NoReturn:
         """
         Called whenever any change has occurred in the data model.
@@ -63,7 +74,7 @@ class LoginScreenView(MDScreen, Observer):
 
         if self.model.data_validation_status:
             self.dialog.dismiss()
-            toast("You have passed the verification successfully!")
+            Clock.schedule_once(self.show_toast, 1)
         if self.model.data_validation_status is False:
             self.dialog.text = "Wrong data!"
             self.dialog.auto_dismiss = True
